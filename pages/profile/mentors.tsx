a18ab334ccc9e7329df524d0f4c81694c6972141
Pugachov-Mihail <xd383727@gmail.com>
2022-10-12 23:19:22 +0300

    mentor list

--- pages/profile/mentors.tsx
+++ pages/profile/mentors.tsx
@@ -4,23 +4,25 @@ import ProfileTitle from "../../components/profile/ProfileTitle";
 import {Avatar, Container, Grid} from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import MentorList from "../../components/profile/MentorList";
 
 const Mentors = () => {
+    const mentor = [
+        {key: 1, text:"Михаил"},
+        {key: 2, text:"Михаил"},
+        {key: 3, text:"Михаил"},
+        {key: 4, text:"Михаил"},
+        {key: 5, text:"Михаил"},
+    ]
     return(
         <BackgoundUser>
             <ProfileTitle text={"Мои ментора"}/>
-            <Grid container xs>
-                <Grid>
-                    <Container>
-                        <Grid xs={1}>
-                            <ArrowBackIosIcon/>
-                        </Grid>
-                        <Grid>
-                            <Avatar/>
-                        </Grid>
-                        <Grid><ArrowForwardIosIcon/></Grid>
-                    </Container>
-                </Grid>
+            <Grid container>
+                <Container>
+                    <Grid xs={10}>
+                        <MentorList mentor={mentor}/>
+                    </Grid>
+                </Container>
             </Grid>
         </BackgoundUser>
     )
