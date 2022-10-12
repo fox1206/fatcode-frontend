b681a65e74009f22d6fd66e86ea9dd52fc95c037
Mari Komardina <fox_kom@internet.ru>
2022-10-12 23:16:35 +0300

    fix: style for a

--- components/header/componentsHeader/HeaderLinks.tsx
+++ components/header/componentsHeader/HeaderLinks.tsx
@@ -10,7 +10,8 @@ const HeaderLinks: React.FC<LinkProps> = ({links})=>{
           display: 'flex',
           alignItems: 'center',
           columnGap: 6,
-          whiteSpace: 'nowrap'
+          whiteSpace: 'nowrap',
+          '& > a:hover': {color: '#69CB2D', transition: '.5s'}
         }}>
           {links.map((item) => (
             <Link key={item.id} href={item.href}>
