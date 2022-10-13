f464aabc04962792e1447ccf69587aac6b67a436
Pugachov-Mihail <xd383727@gmail.com>
2022-10-13 23:47:36 +0300

    create my donats

--- layout/profile/LeftMenu.tsx
+++ layout/profile/LeftMenu.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {Container} from "@mui/material";
 import TextLeftMenu from "./TextLeftMenu";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
 
 const LeftMenu = () => {
+    const pets = true
+
     const list = [
         {key: 1, text: "Профиль", href: "1"},
         {key: 2, text: "Мой питомец", href: "mypet"},
@@ -12,17 +16,34 @@ const LeftMenu = () => {
         {key: 6, text: "Мои заметки", href: "my"},
         {key: 7, text: "Вопрос-ответ", href: "qus"},
     ]
+
+    if (pets){
     return(<>
         <Container sx={{
             background: "#69CB2D",
             borderBottomLeftRadius: 20,
             borderBottomRightRadius: 20,
             paddingTop: 5,
+            paddingBottom: 2
         }}>
             <TextLeftMenu list={list}/>
         </Container>
         </>
-    )
+    )}else {return(<>
+            <Container sx={{
+                background: "#69CB2D",
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20,
+                paddingTop: 5,
+                paddingBottom: 2
+            }}>
+                <TextLeftMenu list={list}/>
+            </Container>
+            <Grid>
+                <Image src={"/assets/image/svg/Laps.svg"} width={200} height={200} />
+            </Grid>
+        </>
+    )}
 }
 
 export default LeftMenu
---
+++ pages/profile/mydonats.tsx
@@ -0,0 +1,64 @@
+import ProfileTitle from "../../components/profile/ProfileTitle";
+import BackgoundUser from "../../layout/ProfileBack/BackgoundUser";
+import {Button, Typography} from "@mui/material";
+import Image from "next/image";
+import Grid from "@mui/material/Grid";
+import PetsDonats from "../../components/profile/PetsDonats";
+
+
+const Mydonats = () => {
+  return(
+      <>
+        <BackgoundUser>
+            <ProfileTitle text={"Мои донаты"}/>
+            <Typography>
+                Всё обучение на сайте предоставляется бесплатно и держится на команде отчаянных энтузиастов, однако, если Вам вдруг хочется подкинуть нашей команде пару копеек на кофе, хлеб и может даже что-то посерьезнее — мы будем бесконечно рады и поделим между всеми членами команды.
+                Но если вдруг Вам не комфортно помогать голодным разрабам, то мы предлагаем альтернативу — помощь животным. Среди нас есть энтузиасты, которые лично выбирают конкретных животных и помогают им оплатить передержки, корм, операции, лекарства и прочее. Следы ваших донатов вы будете видеть на этой странице в качестве коротких фотоотчетов.
+                Фотографии же сытых разрабов, мы вам не предоставим, увы.
+                Удачи в обучении!
+                И не забывайте про эту вкладочку - она дарит волшебство!
+            </Typography>
+            <Button sx={{
+                width: 200,
+                color: "#69CB2D",
+                marginTop: 4,
+                background: "white",
+                fontWeight: "bold",
+                borderRadius: 20,
+                border: "1px solid #69CB2D",
+                "&: hover":{
+                    color: "white",
+                    background: "#69CB2D",
+                    border: "1px solid #69CB2D"
+                }}}>
+                Нашей команде
+            </Button>
+            <Button sx={{
+                marginLeft: 5,
+                width: 200,
+                color: "#69CB2D",
+                marginTop: 4,
+                background: "white",
+                fontWeight: "bold",
+                borderRadius: 20,
+                border: "1px solid #69CB2D",
+                "&: hover":{
+                    color: "white",
+                    background: "#69CB2D",
+                    border: "1px solid #69CB2D"
+                }}}>
+                Животным
+            </Button>
+            <Typography ml={73} sx={{
+                color: "#BBC0CB",
+                fontSize: 15
+            }}>
+                Ваши маленькие чудеса
+            </Typography>
+            <PetsDonats/>
+        </BackgoundUser>
+      </>
+  )
+}
+
+export default Mydonats
